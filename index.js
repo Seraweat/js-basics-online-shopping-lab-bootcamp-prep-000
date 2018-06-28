63c9f423751996045f211c809536be6c3f830dd1
Seraweat Arefaine <seraweat.arefaine@gmail.com>
2018-06-28 17:29:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.random() * Math.floor(100)
+  var cartObj = {itemName: item,
+             itemPrice: price};
+    cart.push(cartObj);
+
+  return item + " has been added to your cart."
 }
 
 function viewCart() {
-  // write your code here
+  var cartStr = []
+  if (getCart().length < 1) {
+    return "Your shopping cart is empty."
+  } else
+  for (var i = 0; i <= getCart().length; i++);
+  cartStr += "In your cart, you have " + getCart()[i].itemName + " at" + " $" + getCart()[i].itemPrice + ", ";
+
+  return cartStr;
 }
 
 function total() {
